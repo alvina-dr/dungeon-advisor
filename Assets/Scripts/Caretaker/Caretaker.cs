07175df6a13347b9fa818395ced5f881458a3640
Alvina <alvina@tavroxgames.com>
2023-07-08 10:45:26 +0200

    player movement

--- Assets/Scripts/Caretaker/Caretaker.cs
+++ Assets/Scripts/Caretaker/Caretaker.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Caretaker : MonoBehaviour
 {
+    #region fields
+    public Vector3 direction;
+    public float speed;
+    public Rigidbody rb;
+    #endregion
+
     void Start()
     {
         
@@ -11,6 +17,12 @@ public class Caretaker : MonoBehaviour
 
     void Update()
     {
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(direction.x * speed, 0, direction.z * speed);
     }
 }
