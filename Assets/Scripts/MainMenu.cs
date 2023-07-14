a1879d1791d02f313c62689d0a0a3d7826d1c0e6
Alvina <alvina@tavroxgames.com>
2023-07-14 16:37:32 +0200

    maj ui minimap + hero dust fight particle WIP

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame() 
     {
-        SceneManager.LoadScene("Alvina"); // load the scene after 0 (mainMenu)
+        SceneManager.LoadScene("Emma"); // load the scene after 0 (mainMenu)
     }
 
     public void FullscreenToggle()
